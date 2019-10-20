0fdde112de835a2786cbb3317862c0d80444dba0
Michael Lulich <michael.lulich@gmail.com>
2019-10-20 16:31:51 -0500

    created header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ShoppingListEditComponent } from './components/shopping-list/shopping-l
 import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './components/recipe-book/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/recipe-detail.component'
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/re
     ShoppingListEditComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
